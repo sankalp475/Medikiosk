1874467762372b515dc029373e4363e2bb9ad072
sankalp475 <sankalp05oct@gmail.com>
2026-09-12 14:17:25 +0530

    new changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { Theme } from "@radix-ui/themes";
-import App from "./App.jsx";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './css/index.css'
+import App from '../src/App.jsx'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Theme accentColor="green" grayColor="olive" radius="large" appearance="light">
-      <App />
-    </Theme>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
